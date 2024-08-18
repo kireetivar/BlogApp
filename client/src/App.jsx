820f7acf7073666b0780cc9af1d9f494dbe01eb3
kireetivar <kireeti3901@gmail.com>
2024-08-18 17:50:48 +0530

    First Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+
+
+function App() {
+
+  return (
+    <>
+      <main className="p-10 max-w-96 m-auto">
+        <header>
+          <a href="">My Blog</a>
+          <nav>
+            <a href="">Login</a>
+            <a href="">Register</a>
+          </nav>
+        </header>
+      </main>
+    </>
+  )
+}
+
+export default App
